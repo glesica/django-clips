df52a84a56ab686810975209cea4298a3cc2abe1
George Lesica <glesica@gmail.com>
2010-12-12 20:47:20 -0600

    Added meta junk.

---
+++ README.md
@@ -0,0 +1,7 @@
+Django-clips
+============
+Django app for cataloging media clips.
+
+About
+-----
+This is the primary app that drives The Economics of Seinfeld.
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-clips',
+    version='0.1.0',
+    description='Primary application for yadayadayadaecon.com.',
+    author='George Lesica',
+    author_email='glesica@gmail.com',
+    url='https://github.com/glesica/django-clips',
+    packages=[
+        'clips',
+        'clips.templatetags',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+    ],
+    requires=[
+        'django',
+    ]
+)
